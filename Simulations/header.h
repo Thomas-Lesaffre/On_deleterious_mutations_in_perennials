8ea6825e079f6d08f630814290e4ec9bc309d2aa
Thomas Lesaffre <48019297+Thomas-Lesaffre@users.noreply.github.com>
2020-05-20 16:48:51 +0200

    Add files via upload
    An error was noticed in the SelRec.cpp file and is now corrected.

---
+++ Simulations/header.h
@@ -0,0 +1,71 @@
+#ifndef DEPRESSION_H
+#define DEPRESSION_H
+
+#include <vector>
+#include <iostream>
+// #include "MersenneTwister.h"
+#include "mt.h"
+//#include <gmp.h>
+//#include <mpfr.h>
+
+using namespace std;
+
+// global variables
+
+#define fichierLecture "parameters.txt"     // names of input
+#define fichierEcriture "results.txt"		// and output files
+
+#define fileMut "mut_save" // back up files.
+#define fileCar "carac_save" 
+
+// definition of structure "chr" representing a chromosome:
+// "M" is the Modifier locus
+// "sel" is a vector containing the positions of deleterious alleles along the chromosome
+
+struct chr
+{
+      vector<double> sel; // Selected loci
+      
+      double S; // 1 = Selfed, 0 = Outcrossed
+      double age; // Age of the individual
+      double sze; // Size of the individual
+      double rep; // Nb. of offspring produced by the individual.
+
+      chr(){}; // Default constructor
+      ~chr(){}; // Default destructor
+};
+
+struct result
+{
+       double pouet;
+};
+
+
+// Prototypes of functions
+
+void ouvrirFichierE();
+
+void open_file_mut();
+void open_file_car();
+
+void transfer_mut(chr * v);
+void transfer_car(chr * v);
+
+void ouvrirFichierS();
+
+bool lireFichier(int &modelr, int &Nr, double &Lr, double &Sr, double &alphar, double &sr, double &maintr, double &prodr, double &hr, double &Ur, int &stepr, double &thrr, int &iterr, int &resume_r, int &save_r, int &freq_save_r);
+
+result recursion(int modelv, int Nv, double Lv, double Sv, double alphav, double sv, double maintv, double prodv, double hv, double Uv, int stepv, double thrv, int iteratv, int resume_v, int save_v, int freq_save_v);
+
+double gammln(const double xx);
+double poisdev(const double xm);
+double binldev(const double pp, const int n);
+double fitness(chr &c1, chr &c2, double wHe, double wHo,  int model);
+
+double growth(chr &c1, chr &c2, double wHe, double wHo, double c, double e, int model);
+
+void rec(chr &res, chr &c1, chr &c2, double sz);
+void cntl_c_handler(int bidon);
+
+
+#endif
